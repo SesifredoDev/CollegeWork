2ac420e606633f1b6b396817b147d9dfba51f47a
SesifredoDev <20160643@student.chelmsford.ac.uk>
2022-03-18 19:12:29 +0000

    about us done, teams started

--- HotBeansWeb/js/main.js
+++ HotBeansWeb/js/main.js
@@ -1,6 +1,6 @@
 var navBar = document.getElementById("nav");
 window.onscroll = function(e){
-    if (window.scrollY > 900){
+    if (window.scrollY > 200){
         navBar.style.background = 'linear-gradient(90deg, rgba(54,36,255,1) 0%, rgba(10,4,84,1) 50%, rgba(143,0,255,1) 100%)';
 
     }else{
