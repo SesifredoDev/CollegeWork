bd4ff341ea35f63ea744f00785460b5721e24c53
SesifredoDev <20160643@student.chelmsford.ac.uk>
2022-03-31 15:30:38 +0000

    31/03/2022

--- HotBeansWeb/js/main.js
+++ HotBeansWeb/js/main.js
@@ -1,7 +1,7 @@
 var navBar = document.getElementById("nav");
 window.onscroll = function(e){
     if (window.scrollY > 200){
-        navBar.style.background = 'linear-gradient(90deg, rgba(54,36,255,1) 0%, rgba(10,4,84,1) 50%, rgba(143,0,255,1) 100%)';
+        navBar.style.background = 'linear-gradient(45deg, rgba(0,0,0,1) 0%, rgba(10,14,10,1) 48%, rgba(29,171,0,1) 100%)';
 
     }else{
         navBar.style.background = 'rgb(68, 68, 68)';
